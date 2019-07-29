19f46332231b9fa0e8a793690a109d798954f2f4
Tiana Johnson <tiana.marie013@gmail.com>
2019-07-29 09:50:27 -0500

    first react commit

---
+++ src/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Header.css';
+
+
+class Header extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1> Spice Wheels </h1>
+            </div>
+        )
+    }
+
+}
+
+export default Header;
