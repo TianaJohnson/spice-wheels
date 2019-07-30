c49f9584e4bed0148c00e98453e086475839909f
Tiana Johnson <tiana.marie013@gmail.com>
2019-07-29 21:59:36 -0500

    template build

---
+++ src/Template/Template.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Template extends Component {
+
+    render() {
+        return (
+            <div>
+                <p>This is a templete for a component build out.</p>
+            </div>
+
+        )
+    }
+
+}
+
+export default Template;
