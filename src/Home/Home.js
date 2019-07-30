6bda30a00715449f8ac34b4ed798c49f77a5ca03
Tiana Johnson <tiana.marie013@gmail.com>
2019-07-29 22:41:49 -0500

    slow motion for me

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Header from './../Header/Header';
 
 class Home extends Component {
 
     render() {
         return (
             <div>
+                <Header/>
                 <p>Home page, Dawg.</p>
             </div>
 
