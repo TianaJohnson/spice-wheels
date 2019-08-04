be3ee642ab36a2cf813c9105fadee30dc9d2911e
Tiana Johnson <tiana.marie013@gmail.com>
2019-08-03 20:34:39 -0500

    form

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -13,6 +13,9 @@ class Contact extends Component {
                 <div className="contact-desc">
                 <h2>Contact us.</h2>
                 <p>Whatever your question, I’m here to help! I think nothing of spending up to an hour researching my customers needs to help them design their dream wheels. Have a question about a build? What to know more about what we do? Want to schedule an appointment to talk about your wheels in person? You’re in the right place!</p>
+                <Form>
+                    
+                </Form>
                 </div>
                 <Footer/>
             </div>
