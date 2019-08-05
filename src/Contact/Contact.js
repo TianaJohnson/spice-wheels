3f3e1c37fec33354ff71afa237518ed9157922d8
Tiana Johnson <tiana.marie013@gmail.com>
2019-08-04 21:49:18 -0500

    name form submit basic react no material

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -8,16 +8,20 @@ class Contact extends Component {
     render() {
         return (
             <div>
-                <Header/>
+                <Header />
                 <h1 className="contact-title">Lets Chat!</h1>
                 <div className="contact-desc">
-                <h2>Contact us.</h2>
-                <p>Whatever your question, I’m here to help! I think nothing of spending up to an hour researching my customers needs to help them design their dream wheels. Have a question about a build? What to know more about what we do? Want to schedule an appointment to talk about your wheels in person? You’re in the right place!</p>
-                <Form>
-                    
-                </Form>
+                    <h2>Contact us.</h2>
+                    <p>Whatever your question, I’m here to help! I think nothing of spending up to an hour researching my customers needs to help them design their dream wheels. Have a question about a build? What to know more about what we do? Want to schedule an appointment to talk about your wheels in person? You’re in the right place!</p>
+                    <form>
+                        <label>
+                            Name:
+    <input type="text" name="name" />
+                        </label>
+                        <input type="submit" value="Submit" />
+                    </form>
                 </div>
-                <Footer/>
+                <Footer />
             </div>
 
         )
