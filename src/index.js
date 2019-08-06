a793479da680aaaeef24b8db1769e1bee53d6374
Tiana Johnson <tiana.marie013@gmail.com>
2019-08-06 13:52:06 -0500

    moving to a diffrent filr to spike

--- src/index.js
+++ src/index.js
@@ -4,9 +4,23 @@ import './index.css';
 import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const express = require('express')
+const bodyParser = require('body-parser')
+const nodemailer = require('nodemailer' )
+const app = express()
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false}))
+
+app.post('/api/form', (req, res) => {
+    console.log(req.body)
+})
+
+const
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
